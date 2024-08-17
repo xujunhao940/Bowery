61d576251da56c396217025af5fca2ee62343a64
许峻豪 <xjhxzywy@gmail.com>
2024-08-17 20:07:46 +0800

    Support searching for answers on the Internet

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import base64
 import io
 import google.generativeai as genai
 from config import *
+from search import searchSchema, search
 
 if api_token == "YOUR_API_TOKEN":
     raise Exception("Replace `YOUR_API_TOKEN` with your Gemini API token in `config.py`")
@@ -19,7 +20,8 @@ generation_config = genai.GenerationConfig(
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash-latest",
     generation_config=generation_config,
-    system_instruction="You are an assistant called Bowery. Answer in the language the user speaks."
+    system_instruction="You are an assistant called Bowery. You don’t have to ask when the user doesn’t ask to search. Search only when the user explicitly states that they want to search. Answer in the language the user speaks.",
+    tools=[searchSchema]
 )
 
 chat_session = model.start_chat()
@@ -35,15 +37,29 @@ def chat():
     def stream():
         data = json.loads(request.data)
         if data["image"] == "Undefined":
-            response = chat_session.send_message(data["message"])
+            response = chat_session.send_message(data["message"], stream=True)
         else:
             img_data = base64.b64decode(data["image"])
             image = Image.open(io.BytesIO(img_data))
-            response = chat_session.send_message([data["message"], image])
+            response = chat_session.send_message([data["message"], image], stream=True)
 
         for chunk in response:
-            yield json.dumps({"type": "text", "message": chunk.text})
-            yield "-|BOWERY SPLIT|-"
+            for part in chunk.parts:
+                if len(part.text) != 0:
+                    print(part.text)
+                    yield json.dumps({"type": "text", "message": part.text})
+                    yield "-|BOWERY SPLIT|-"
+
+        for part in response.parts:
+            if len(part.text) == 0:
+                if part.function_call.name == "search":
+                    print("search")
+                    prompt = search(part.function_call.args["keywords"], part.function_call.args["question"])
+
+                    response = model.generate_content(prompt, stream=True)
+                    for chunk in response:
+                        yield json.dumps({"type": "text", "message": chunk.text})
+                        yield "-|BOWERY SPLIT|-"
 
     return stream_with_context(stream())
 
